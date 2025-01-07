3089c62e9f583aa17345751a86be41633cc15099
FaisalAnsari <faisalansari30121998@gmail.com>
2025-01-07 18:11:24 +0530

    sticky-issue-fixed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-purple_primary text-white py-6 sticky top-0 z-30">
+    <nav className="bg-purple_primary text-white py-6 sticky top-0 z-50 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Left Part */}
