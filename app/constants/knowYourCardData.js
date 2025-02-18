b08f1cee70d2fc1256496ee469045fa7e8fbbcff
FaisalAnsari <faisalansari30121998@gmail.com>
2025-02-18 19:33:10 +0530

    know-your-card

--- app/constants/knowYourCardData.js
+++ app/constants/knowYourCardData.js
@@ -6,9 +6,14 @@ const items=[
         training_requirement:'NVQ Enrollment'
     },
     {
+        card_type:'Red Experienced worker',
+        test_requirement:'Operative test',
+        image_path:'/red-experienced-worker-card-img.png',
+        training_requirement:'NVQ Enrollment'
+    },
+    {
   
-
-        card_type:'Red Experienced worker (Temporary card)',
+        card_type:'Red Provisional Worker (Temporary card)',
         test_requirement:'Operative test',
         image_path:'/red-experienced-worker-card-img.png',
         training_requirement:'NVQ Enrollment'
@@ -17,25 +22,25 @@ const items=[
         card_type:'Red Experienced technicial, supervisor or manager',
         test_requirement:'Specialist OR Manager & Professional Test',
         image_path:'/red-supervisor-card-img.png',
-        training_requirement:'NVQ Level4  or above (Enrollment)'
+        training_requirement:'NVQ Level 4  or above'
     },
     {
         card_type:'Blue Skilled Worker',
         test_requirement:'Operative OR Specialist Test',
         image_path:'/blue-card-img.png',
-        training_requirement:'NVQ Level2'
+        training_requirement:'NVQ Level 2'
     },
     {
         card_type:'Gold Advanced Craft Supervisory',
         test_requirement:'Operative OR Specialist Test',
         image_path:'/gold-advanced-card-img.png',
-        training_requirement:'NVQ Level2 (Enrollment)'
+        training_requirement:'NVQ Level 3'
     },
     {
         card_type:'Black Manager card',
         test_requirement:'Manager & Professional Test',
         image_path:'/black-manager-card-img.png',
-        training_requirement:'NVQ Level4'
+        training_requirement:'NVQ Level 4 / Level 5 / Level 6'
     },
     {
         card_type:'White Professionally Qualified Person',
@@ -44,16 +49,16 @@ const items=[
         training_requirement:'Members of governing body'
     },
     {
-        card_type:'Academically Qualified Person',
+        card_type:'White Academically Qualified Person',
         test_requirement:'Manager & Professional Test',
         image_path:'/white-academically-qualified-card-img.png',
-        training_requirement:'NVQ Level2 (Enrollment)'
+        training_requirement:'NVQ Level 2 (Enrollment)'
     },
     {
         card_type:'Labourer Card',
         test_requirement:'Operative test',
         image_path:'/green-card-img.png',
-        training_requirement:'Level 1 / SCQF Level 4 Awand in Health & Safety in a constructionn environment'
+        training_requirement:'QCF Level 1 / SCQF Level 4 Award in Health & Safety in a constructionn environment, Site safety plus'
     }
 ]
 
