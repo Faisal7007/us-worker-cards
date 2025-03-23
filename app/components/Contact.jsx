309b2d792648bf0053e6fb0d88b99a4515d1df60
uc-SJ <junaid.husain@ucertify.com>
2025-03-23 21:10:45 +0530

    Fix Issue

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -182,7 +182,7 @@ function Contact({ no_banner }) {
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
-                  allowFullScreen=""
+                  // allowFullScreen=""
                   loading="lazy"
                   title="Google Map"
                 ></iframe>
