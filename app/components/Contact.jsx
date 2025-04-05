c2d4bfc2f78dc7a999a2109f4cca258f115f1afd
uc-SJ <junaid.husain@ucertify.com>
2025-04-05 21:06:46 +0530

    Issues Fixed

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -177,7 +177,7 @@ function Contact({ no_banner }) {
               <div className="w-full h-48 sm:h-80 lg:h-96 overflow-hidden">
 
 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2488.5566585166084!2d-0.18871782387861788!3d51.411200317727136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487607004b00d293%3A0x4e5b71cce0946809!2s9%20Dalton%20House!5e0!3m2!1sen!2sin!4v1741453322175!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2488.5566585166084!2d-0.18871782387861788!3d51.411200317727136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487607004b00d293%3A0x4e5b71cce0946809!2s9%20Dalton%20House!5e0!3m2!1sen!2sin!4v1741453322175!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
 
                   width="100%"
                   height="100%"
