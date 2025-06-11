4c5c76e8fb6fd9112d6325f4bbfefc3b1a4ec1f6
Shivang Singh <shivangsingh2240@gmail.com>
2025-06-12 00:00:26 +0530

    UI Bugs

--- app/components/WhatsAppOverlay.jsx
+++ app/components/WhatsAppOverlay.jsx
@@ -9,9 +9,21 @@ export default function WhatsAppOverlay() {
     if (!visible) return null;
 
     return (
-        <div className="fixed bottom-4 right-4 z-[1000] bg-white shadow-md border border-gray-200 p-2 rounded-lg max-w-[220px] text-xs">
-            <div className="flex flex-col items-start gap-1 relative">
-                <p className="font-semibold text-gray-800">Get Consultation</p>
+        <div className="fixed bottom-4 right-4 z-[1000] bg-white shadow-md border border-gray-200 p-2 rounded-lg max-w-[320px] text-xs">
+            <div className="flex flex-col items-center gap-2 relative">
+                <div className="flex flex-row items-start gap-2">
+                    <p className="font-semibold text-gray-800">Get Consultation
+                    </p>
+
+                    <button
+                        className=" top-0 right-0 text-black hover:text-gray-700 text-xs"
+                        onClick={() => setVisible(false)}
+                    >
+                        ✕
+                    </button>
+
+                </div>
+
                 <a
                     href="https://wa.me/447883317237?text=Hi%20there!%20I%20need%20help%20with%20Construction%20Card%20Services."
                     target="_blank"
@@ -21,12 +33,6 @@ export default function WhatsAppOverlay() {
                     <IoLogoWhatsapp className="text-base" />
                     Send Hi
                 </a>
-                <button
-                    className="absolute top-0 right-0 text-black hover:text-gray-700 text-xs"
-                    onClick={() => setVisible(false)}
-                >
-                    ✕
-                </button>
             </div>
         </div>
     );
